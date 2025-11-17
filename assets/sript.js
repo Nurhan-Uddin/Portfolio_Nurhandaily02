a5364a9d87c6ccc2f02f7873c9ce6e895f329cad
unknown <nurhandaily@gmail.com>
2025-11-17 10:08:57 +0600

    noor

--- assets/sript.js
+++ assets/sript.js
@@ -17,3 +17,35 @@ resumeDetails[idx].classList.add('active');
 });
 });
 
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabcursor:true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  /* Responsive */
+breakpoints: {
+  0: {
+    slidesPerView: 1
+  },
+  768: {
+    slidesPerView: 2
+  },
+  1024: {
+    slidesPerView: 3
+  }
+}
+
+});
